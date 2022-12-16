a14e9d472ee17268d2a0913f57def706d2a3b54f
Francesca Santoriello <f.santoriello@hotmail.com>
2022-12-16 14:44:12 +0100

    heroku fix

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -3,14 +3,12 @@ require 'securerandom'
 class AgreementsController < ApplicationController
   def index
     id = params[:id]
-    
-    if id == nil
-      redirect_to '/'
-    end
+    redirect_to '/' if id == nil
     
     client = Client.new().create_client()
     uuid = SecureRandom.uuid
-    redirect_url = results_path
+    redirect_url = "https://wallet-app.herokuapp.com/results" if Rails.env.production?
+    redirect_url = "http://localhost:3000/results/" if Rails.env.development?
     init = client.init_session(redirect_url: redirect_url, institution_id: id, reference_id: uuid)
     session[:requisition_id] = init["id"]
     redirect_to init["link"], allow_other_host: true
